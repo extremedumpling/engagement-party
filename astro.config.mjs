a75bdedb8ba4cf01c8eff195da515eddbcbd4874
Jaimie Chow <jaimiechow@MacBookPro.mynet>
2026-06-17 23:42:35 +0100

    Finished content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: "Nanum Myeongjo",
+      cssVariable: "--font-nanum-myeongjo",
+    },
+  ],
+});
